93391c14a6cf22c40266555735fbc27f24691fae
CrowdSense <admin@DevBook.home>
2010-01-07 14:31:54 +0000

    updated setup with author/email

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
-#!/usr/bin/python2.6
-
 from setuptools import setup, find_packages
 
-setup(
-	name='django-ab',
-	packages=find_packages(),
+setup(name="django-ab",
+           version="0.1",
+           description="A simple AB Testing app for Django!",
+           author="John Boxall",
+           author_email="john@handimobility.ca",
+           packages=find_packages(),
+           include_package_data=True,
 )
